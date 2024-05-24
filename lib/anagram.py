7d1c7a826b26ed73a393e1845025fd642273bf3c
whitney <shisiawhitney215@gmail.com>
2024-05-24 09:56:05 +0300

    py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram :
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            sorted_candidate = ''.join(sorted(candidate))
+            if sorted_candidate == self.sorted_word:
+                matches.append(candidate)
+        return matches
